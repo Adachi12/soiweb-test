65904c753cd57f9330d71802fe2b814c2fb5759a
Adachi12 <42488400+Adachi12@users.noreply.github.com>
2021-11-19 17:18:49 +0900

    Add files via upload

---
+++ Mobile-ver/models/settings.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+#coding:utf-8
+
+DBSETTINGS = dict(
+   db_name = 'postgres',
+   db_host = 'localhost',
+   db_user = 'workuser',
+   db_password = 'santana0120',
+   db_port = 5432,
+   db_encode = 'utf8',
+)
